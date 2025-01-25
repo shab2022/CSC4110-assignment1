687e660d1a543b537cb0ac7b354f729be649d9ff
Shahab Alriyashi <nerdam2015@gmail.com>
2025-01-24 20:49:25 -0500

    Adding author name

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,4 +1,5 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+// Author: Shahab Alriyashi <hl0238@wayne.edu>
 // Header file for input output functions 
 #include<iostream>  
   
